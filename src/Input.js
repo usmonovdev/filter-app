3066eaa27eb5a5c052d0e92e35167d99c281dea1
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-01-06 17:03:22 +0500

    Checkbox added function not working
    Checkbox function not working!

--- src/Input.js
+++ src/Input.js
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
-import Result from './Result';
+import { useEffect } from 'react';
 import Market from './Market';
+import Result from './Result';
 
 function Input() {
     const [input, setInput] = useState("");
+    const [stockCheck, setStockCheck] = useState(false)
 
     const filterItems = () => {
         return Market.filter(e => e.name.toLowerCase().includes(input))
     }
 
+    useEffect(() => {
+        const filterStock = (st) => {
+            return st.stocked === true
+        }
+        if (stockCheck) {
+            Market.filter(filterStock);
+        } else {
+            console.log("xato");
+        }
+    }, [Market])
+
+    console.log(stockCheck)
+
+    // useEffect(() => {
+    //     const filterStock = (st) => {
+    //         return st.stocked === true
+    //     }
+    //     setStock(Market.filter(filterStock))
+    // }, [])
+    // console.log(stock);
+
     return (
         <div className='input'>
-            <h3>Filter items</h3>
-            <input type="text" placeholder='Search...' onChange={(e) => setInput(e.target.value.toLowerCase())} />
+            <input type="text" placeholder='Search...' onChange={(e) => setInput(e.target.value.toLowerCase())} /> <br />
+            <input type="checkbox" id='checkbox' onChange={(e) => setStockCheck(e.target.checked)} />
+            <label htmlFor="checkbox">Only show products in stock</label>
             <Result result={filterItems()} />
         </div>
     )
